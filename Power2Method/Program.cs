0c5807b5474fcca2591bf66c43c9b7f33f9f9c71
Mikapuccino <nelsonfcmilheiro@gmail.com>
2023-03-22 17:27:39 +0000

    Add Powerof2Until5 method to program

---
+++ Power2Method/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Power2Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        // Method writes the value of i to the power of 1 through 5
+        private static void PowersOf2Until5()
+        {
+            for(int i = 1; i <= (1 << 5); i << 1)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
