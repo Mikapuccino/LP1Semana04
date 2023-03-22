075d632381a489a2993a2953fd9ef5896622d3c6
Mikapuccino <nelsonfcmilheiro@gmail.com>
2023-03-22 17:39:33 +0000

    Calls method twice, program fully functional, weird error when incrementing i within the for loop, works with incrementing after writing i

--- Power2Method/Program.cs
+++ Power2Method/Program.cs
@@ -6,15 +6,17 @@ namespace Power2Method
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            PowersOf2Until5();
+            PowersOf2Until5();
         }
 
         // Method writes the value of i to the power of 1 through 5
         private static void PowersOf2Until5()
         {
-            for(int i = 1; i <= (1 << 5); i << 1)
+            for(int i = 1; i <= (1 << 5); )
             {
                 Console.WriteLine(i);
+                i = i << 1;
             }
         }
     }
