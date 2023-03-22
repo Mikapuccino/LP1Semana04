1862017a497c0f542b1c6d1f8f77b544c66a473e
Mikapuccino <nelsonfcmilheiro@gmail.com>
2023-03-22 18:04:56 +0000

    Add XML comments to program

--- Power2Method/Program.cs
+++ Power2Method/Program.cs
@@ -2,8 +2,19 @@
 
 namespace Power2Method
 {
+    /// <summary>
+    /// Asks user for a number and calls the PowersOf2UntilN method
+    /// passing that number as the argument, then calls that method
+    /// two more times passing 4 and 8 as the arguments
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// Main method responsible of calling the other methods with 
+        /// the appropriate arguments
+        /// </summary>
+        /// <param name="args">Arguments passed by the user when running
+        /// the program</param>
         static void Main(string[] args)
         {
             // Asks user for a number for later use
@@ -12,20 +23,25 @@ namespace Power2Method
 
             // Calls method passing the number the user gave as the argument
             PowersOf2UntilN(nCount);
-            PowersOf2UntilN(nCount);
+
             // Calls method with specific values
             PowersOf2UntilN(4);
             PowersOf2UntilN(8);
         }
 
-        // Method calls PowersOf2UntilN method passing 5 as the argument
+        /// <summary>
+        /// Calls PowersOf2UntilN method passing 5 as the argument
+        /// </summary>
         private static void PowersOf2Until5()
         {
             PowersOf2UntilN(5);
         }
 
-        // Method writes the value of i to the power of 1 through n,
-        // n being specified by the user
+        /// <summary>
+        /// Writes the value of i to the power of 1 through n
+        /// </summary>
+        /// <param name="n">Number that determines number of times the for
+        /// loop repeats</param>
         private static void PowersOf2UntilN(int n)
         {
             for(int i = 1; i <= (1 << n); i = i << 1)
