c26d529e33d6f4e45cc9cf095b17ff1aa13ad53c
Mikapuccino <nelsonfcmilheiro@gmail.com>
2023-03-22 18:38:07 +0000

    Lucas method returns number in the position specified by the user, add XML comments to Lucas method

--- Lucas/Program.cs
+++ Lucas/Program.cs
@@ -8,5 +8,28 @@ namespace Lucas
         {
             Console.WriteLine("Hello, World!");
         }
+
+        /// <summary>
+        /// Returns value of lc which is the number in the Lucas sequence
+        /// in the position specified by the user
+        /// </summary>
+        /// <param name="n">Position of the number
+        /// in the Lucas sequence</param>
+        /// <returns>Number in the specified position
+        /// of the Lucas sequence</returns>
+        private static int Lucas(int n)
+        {
+            int lc;
+            if (n == 0)
+                lc = 2;
+            
+            else if (n == 1)
+                lc = 1;
+
+            else
+                lc = Lucas(n - 1) + Lucas(n - 2);
+
+            return lc;
+        } 
     }
 }
